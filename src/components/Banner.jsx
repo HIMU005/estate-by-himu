a81dc0fa337ffcf686b33846be418df9cb3acc86
HIMU005 <hashanuzzaman99@gamil.com>
2024-04-16 12:57:34 +0600

    swiperjs added in the banner now time for designing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,31 +1,61 @@
+import { useContext, useRef } from 'react';
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-// import { useContext } from 'react';
-// import { AuthContext } from '../routes/provider/AuthProvider';
-// import Slide from './Slide';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { AuthContext } from '../routes/provider/AuthProvider';
+import Slide from './Slide';
+
 
 const Banner = () => {
-    // const { fakeData } = useContext(AuthContext);
-    // console.log(typeof fakeData);
+    const { fakeData } = useContext(AuthContext)
+
+    const progressCircle = useRef(null);
+    const progressContent = useRef(null);
+    const onAutoplayTimeLeft = (s, time, progress) => {
+        progressCircle.current.style.setProperty('--progress', 1 - progress);
+        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    };
     return (
         <div>
             <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-            // onSwiper={(swiper) => console.log(swiper)}
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                onAutoplayTimeLeft={onAutoplayTimeLeft}
+                className="mySwiper"
             >
+                {
+                    fakeData.map(fakeDatum => <SwiperSlide key={fakeDatum.id}>
+                        <Slide fakeDatum={fakeDatum}></Slide>
+                    </SwiperSlide>)
+                }
 
-
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                ...
+                {/* timer part here */}
+                <div className="autoplay-progress w-16 h-16" slot="container-end">
+                    <svg viewBox="0 0 48 48" ref={progressCircle}>
+                        <circle cx="24" cy="24" r="20"></circle>
+                        <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fill="#fff" className='text-xs'>
+                            <tspan ref={progressContent}></tspan>
+                        </text>
+                    </svg>
+                </div>
             </Swiper>
-        </div>
+        </div >
     );
 };
 
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,10 +1,34 @@
+import PropTypes from 'prop-types';
 
-const Slide = () => {
+const Slide = ({ fakeDatum }) => {
+
+    const {
+        estate_title,
+        segment_name,
+        description,
+        price,
+        status,
+        area,
+        location,
+        facilities,
+        image,
+    } = fakeDatum;
+
     return (
-        <div>
-            <h2 className="text-3xl">I am a slide</h2>
+        <div className="border-2 h-96" style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+            <h2 className="text-3xl">{estate_title}</h2>
+            <h2 className="text-xl">{segment_name}</h2>
+            <p className="dark:text-gray-800">{description.slice(0, 150)} </p>
         </div>
     );
 };
 
 export default Slide;
+Slide.propTypes = {
+
+    id: PropTypes.number,
+    fakeDatum: PropTypes.object,
+    image: PropTypes.string,
+    estate_title: PropTypes.string,
+    segment_name: PropTypes.string,
+}
